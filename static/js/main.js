a7505d682d7cf4ca743918858efb9a0f94b34b72
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2018-10-23 21:28:54 -0400

    skeleton of website is made

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,32 @@ $(document).ready(function(){
 	        scrollTop: $("#services").offset().top},
 	        1300);
     });
+
+    $('.dropbutton').click(function () {
+        document.getElementById("myDropDown").classList.toggle("show");
+    });
+
+    window.onclick = function(e) {
+        if (!e.target.matches('.dropbutton')) {
+            var myDropdown = document.getElementById("myDropDown");
+            if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+            }
+        }
+    }
+
+    // $('#contact').height(
+    //     $("#logo").height()
+    // );
+
+    // $('#brand').height(
+    //     $("#logo").height()
+    // );
+
+    // $('#brand').height(
+    //     $("#logo").height()
+    // );
+    // $('#top').height(
+    //     $("#logo").height() + $("#main").height()
+    // );
 });
