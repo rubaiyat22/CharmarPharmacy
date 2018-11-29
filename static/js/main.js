94e8ee69996c6082bf462fb0bc7f1667a57ad711
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2018-11-28 21:48:30 -0500

    almost finished product

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,51 @@
 $(document).ready(function(){
+    carousel = new Siema({selector: '#carousel', loop: true})
+    setInterval(() => carousel.next(), 5000)
+
     $("#downimg").click(function(){
     	$('html,body').animate({
 	        scrollTop: $("#services").offset().top},
-	        1300);
+	        1200);
     });
 
-    $('#dropbutton').click(function () {
-        document.getElementById("myDropDown").classList.toggle("show");
-    });
-
-    window.onclick = function(e) {
-        if (!e.target.matches('#dropbutton')) {
-            var myDropdown = document.getElementById("myDropDown");
-            if (myDropdown.classList.contains('show')) {
-                myDropdown.classList.remove('show');
-            }
+    $(window).click(function(e){
+        if (!e.target.matches('#dropbutton') && $('#myDropDown').is(':visible')) {
+            $('#myDropDown').toggle();
         }
+    });
+
+    $('#dropbutton').click(function(){
+        $('#myDropDown').toggle();
+    });
+
+    $(window).resize(function(){
+        if ($('#contact').is(':visible')) {
+            $('#brand_img').css('margin-top', $('#contact').height()/4);
+        } else {
+            $('#brand_img').css('margin-top', '2%');
+            $('#dropbutton').css('margin-top', $('#brand_img').height()/4);
+        }
+    });
+    if ($('#contact').is(':visible')) {
+        $('#brand_img').css('margin-top', $('#contact').height()/4);
+    } else {
+        $('#brand_img').css('margin-top', '2%');
+        $('#dropbutton').css('margin-top', $('#brand_img').height()/4);
     }
-
-    // $('#contact').height(
-    //     $("#logo").height()
-    // );
-
-    // $('#brand').height(
-    //     $("#logo").height()
-    // );
-
-    // $('#brand').height(
-    //     $("#logo").height()
-    // );
-    // $('#top').height(
-    //     $("#logo").height() + $("#main").height()
-    // );
 });
+
+function scrollFunc(elmnt){
+    let time = 0;
+    if (elmnt == 'services'){
+        time = 800;
+    }
+    else if (elmnt == 'pharmacists'){
+        time = 1200;
+    }
+    else {
+        time = 1500;
+    }
+    $('html,body').animate({
+        scrollTop: $(document.getElementById(elmnt)).offset().top}, time);
+    $(document.getElementById(elmnt)).fadeIn('slow');
+}
